8303bffc9f5091503259fa2ce1d42308a90a7ac9
dogpoopy <qtqtron@gmail.com>
2024-07-27 16:11:40 +0800

    Refactor and switch to independent build scripts

--- scripts/crdroid.sh
+++ scripts/crdroid.sh
@@ -8,7 +8,7 @@ crave run --no-patch -- "
 source build/envsetup.sh && \
 brunch apollo"
 
-#rm -rf .repo/local_manifests && \
+#rm -rf .repo/local_manifests ; \
 #repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 14.0 -g default,-mips,-darwin,-notdefault && \
 #git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-cr14 .repo/local_manifests && \
 
---
+++ scripts/evo.sh
@@ -0,0 +1,21 @@
+device="apollo" # required
+project="Evolution-XYZ" # just for telegram notif
+
+crave ssh -- "rm -f out/target/product/$device/*.zip"
+
+crave run --no-patch -- "
+rm -rf .repo/local_manifests ; \
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-XYZ/manifest -b udc -g default,-mips,-darwin,-notdefault && \
+git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-evo14 .repo/local_manifests && \
+/opt/crave/resync.sh && \
+source build/envsetup.sh && \
+lunch lineage_apollo-userdebug && \
+make installclean && \
+m evolution"
+
+#rm -rf .repo/local_manifests ; \
+#repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-XYZ/manifest -b udc -g default,-mips,-darwin,-notdefault && \
+#git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-evo14 .repo/local_manifests && \
+
+echo "DEVICE_CODENAME=$device" >> $GITHUB_ENV
+echo "ROM_NAME=$project" >> $GITHUB_ENV
