be81059a06d9951f9529869bcf10f85334c104aa
dogpoopy <qtqtron@gmail.com>
2024-08-13 16:52:54 +0800

    Add projects base support

--- scripts/pos.sh
+++ scripts/pos.sh
@@ -7,6 +7,9 @@ echo "ROM_NAME=$project" >> $GITHUB_ENV
 crave ssh -- "rm -f out/target/product/$device/*.zip"
 
 crave run --no-patch -- "
+rm -rf .repo/local_manifests ; \
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen -g default,-mips,-darwin,-notdefault && \
+git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-pos14 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 source build/envsetup.sh && \
 lunch aosp_apollo-ap2a-user && \
