13cc4d12e13bf7e96527e7875a3239c4a17f7681
dogpoopy <dogpoopy.git@gmail.com>
2024-09-03 02:10:14 +0800

    Update pos.sh

--- scripts/pos.sh
+++ scripts/pos.sh
@@ -4,7 +4,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest
 git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-pos14 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 source build/envsetup.sh && \
-lunch aosp_apollo-ap2a-userdebug && \
+lunch aosp_apollo-ap2a-user && \
 make installclean && \
 mka bacon > /dev/null"
 
