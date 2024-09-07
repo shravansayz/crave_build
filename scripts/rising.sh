f3e21f932acf751bba37a375869ab342892d8285
dogpoopy <dogpoopy.git@gmail.com>
2024-09-07 22:52:19 +0800

    Update rising.sh

--- scripts/rising.sh
+++ scripts/rising.sh
@@ -1,13 +1,19 @@
 crave run --no-patch -- "
 rm -rf .repo/local_manifests ; \
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b fourteen -g default,-mips,-darwin,-notdefault && \
-git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-cr14 .repo/local_manifests && \
+git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-rising14 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 source build/envsetup.sh && \
 riseup apollo userdebug && \
+rise b > /dev/null && \
+mv out/target/product/apollo/RisingOS*.zip . && \
 export WITH_GMS=true && \
+export TARGET_DEFAULT_PIXEL_LAUNCHER=true && \
+rise b > /dev/null && \
+mv out/target/product/apollo/RisingOS*.zip . && \
 export TARGET_CORE_GMS=true && \
 export TARGET_CORE_GMS_EXTRAS=true && \
-export TARGET_DEFAULT_PIXEL_LAUNCHER=true && \
-rise b > /dev/null
+rise b > /dev/null && \
+mv out/target/product/apollo/RisingOS*.zip .
 "
+exit 1
