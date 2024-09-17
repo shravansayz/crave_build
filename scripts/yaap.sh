f6975a17b10bdec9d89b32e986ddece380db1ca2
dogpoopy <dogpoopy.git@gmail.com>
2024-09-17 10:06:52 +0800

    Update yaap.sh

--- scripts/yaap.sh
+++ scripts/yaap.sh
@@ -1,5 +1,5 @@
 rm -rf .repo/local_manifests && \
-repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b fourteen -git-lfs -g default,-mips,-darwin,-notdefault && \
+repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b fifteen -git-lfs -g default,-mips,-darwin,-notdefault && \
 git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-yaap15 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 export TARGET_BUILD_GAPPS=true ; \
