f856aefc2fea7809cfea5ff2af9377e9964dbf5c
dogpoopy <dogpoopy.git@gmail.com>
2024-09-21 08:50:26 +0800

    Update evox.sh

--- scripts/evox.sh
+++ scripts/evox.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-XYZ/manifes
 git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-evo14 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 repo forall -c 'git lfs install && git lfs pull' ; \
+export RELAX_USES_LIBRARY_CHECK=true && \
 source build/envsetup.sh && \
 lunch lineage_apollo-user && \
 make installclean && \
