845ec848bd2d128b5590d01f8c671932812b4eaa
dogpoopy <dogpoopy.git@gmail.com>
2024-10-07 19:58:56 +0800

    Update rising.sh

--- scripts/rising.sh
+++ scripts/rising.sh
@@ -2,8 +2,9 @@ rm -rf .repo/local_manifests ; \
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b fourteen -g default,-mips,-darwin,-notdefault && \
 git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-rising14 .repo/local_manifests && \
 /opt/crave/resync.sh && \
+export BUILD_RISING=true
 source build/envsetup.sh && \
-riseup apollo userdebug && \
+riseup apollo user && \
 rise b > /dev/null && \
 mv out/target/product/apollo/RisingOS*.zip . && \
 export WITH_GMS=true && \
