b4075f390d6c7a0d7ab58e815a11e407d36c1a52
dogpoopy <dogpoopy.git@gmail.com>
2024-10-14 16:08:11 +0800

    Update scripts

---
+++ scripts/derp.sh
@@ -0,0 +1,9 @@
+rm -rf .repo/local_manifests && \
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 15 -git-lfs -g default,-mips,-darwin,-notdefault && \
+git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-derp15 .repo/local_manifests && \
+/opt/crave/resync.sh && \
+repo forall -c 'git lfs install && git lfs pull' ; \
+source build/envsetup.sh && \
+lunch derp_apollo-user && \
+make installclean ; \
+mka derp
