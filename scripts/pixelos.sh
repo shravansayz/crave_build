22e3d25592140ec0ef2250ff10415e768ea52dc2
dogpoopy <dogpoopy.git@gmail.com>
2024-10-29 20:45:42 +0800

    Update pixelos.sh
    * Fixes for git lfs issue

--- scripts/pixelos.sh
+++ scripts/pixelos.sh
@@ -1,7 +1,8 @@
 rm -rf .repo/local_manifests ; \
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen -g default,-mips,-darwin,-notdefault && \
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs -g default,-mips,-darwin,-notdefault && \
 git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-pos14 .repo/local_manifests && \
 /opt/crave/resync.sh && \
+repo forall -c 'git lfs pull' ; \
 source build/envsetup.sh && \
 lunch aosp_apollo-ap2a-user && \
 make installclean ; \
