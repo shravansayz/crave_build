af7068f905e0332da35df8efa31a1d247e6dde21
Shravankumar <shravankumarsayz@gmail.com>
2024-11-15 17:32:19 +0530

    Update derp.sh

--- scripts/derp.sh
+++ scripts/derp.sh
@@ -1,12 +1,12 @@
 rm -rf .repo/local_manifests && \
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --git-lfs -g default,-mips,-darwin,-notdefault && \
-git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-derp15 .repo/local_manifests && \
+git clone https://github.com/shravansayz/local_manifests.git -b derp15 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 repo forall -c 'git lfs pull' ; \
 export DISABLE_STUB_VALIDATION=true ; \
-export TZ=Asia/Manila ; \
-cd vendor/derp && curl -s https://raw.githubusercontent.com/aosp-realm/ota-stuff/refs/heads/main/0001-tools-Generate-json-for-non-official-builds.patch > json.patch && git apply json.patch ; cd ../../ && \
+export TZ=Asia/Kolkata ; \
+cd vendor/derp && curl -s https://raw.githubusercontent.com/shravansayz/ota-stuff/refs/heads/main/0001-tools-Generate-json-for-non-official-builds.patch > json.patch && git apply json.patch ; cd ../../ && \
 source build/envsetup.sh && \
-lunch derp_apollo-user && \
+lunch derp_RMX1901-user && \
 make installclean ; \
 mka derp
