b4e88b36be68335a893e33e98b82b1d878faaf9b
dogpoopy <dogpoopy.git@gmail.com>
2024-11-15 12:31:44 +0800

    Update derp.sh

--- scripts/derp.sh
+++ scripts/derp.sh
@@ -5,7 +5,7 @@ git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-der
 repo forall -c 'git lfs pull' ; \
 export DISABLE_STUB_VALIDATION=true ; \
 export TZ=Asia/Manila ; \
-cd vendor/derp && curl -s https://raw.githubusercontent.com/aosp-realm/ota-stuff/refs/heads/main/0001-tools-Allow-json-to-be-generated-in-all-builds.patch > json.patch && git apply json.patch ; cd ../../ && \
+cd vendor/derp && curl -s https://raw.githubusercontent.com/aosp-realm/ota-stuff/refs/heads/main/0001-tools-Generate-json-for-non-official-builds.patch > json.patch && git apply json.patch ; cd ../../ && \
 source build/envsetup.sh && \
 lunch derp_apollo-user && \
 make installclean ; \
